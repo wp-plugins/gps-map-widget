2ca646af5508aa38e26aad5ea57118230c88661f
jondor <jondor@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-31 20:55:36 +0000

    Added database support and two shortcodes
    git-svn-id: https://plugins.svn.wordpress.org/gps-map-widget/trunk@1017664 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- GPS_MAP_Widget.php
+++ GPS_MAP_Widget.php
@@ -12,6 +12,96 @@ Version: 1.0
 Author URI: http://www.funsite.eu/
 */
 
+// Add Shortcode
+
+function getLocationFromDBorExif($post_thumbnail_id) {
+	// Check if the location is already stored in the database
+	// if not, try to get it from the EXIF information and store it.
+	$location = get_post_meta($post_thumbnail_id,'EXIF_location',true);
+	if (empty($location)) {
+		$thumbnail=get_attached_file( $post_thumbnail_id, true );
+		$exif = exif_read_data($thumbnail);
+		if (is_array($exif["GPSLatitude"]) && is_array($exif["GPSLongitude"])) {
+			$location['latitude'] = $this->gps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+			$location['longitude'] = $this->gps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+			$location['hasLocation'] = true;
+		} else {
+			$location['hasLocation'] = false;
+		}
+		add_post_meta($post_thumbnail_id,'EXIF_location',$location) || update_post_meta($post_thumbnail_id,'EXIF_location',$location);
+	}
+return $location;
+}
+
+
+function custom_EXIF_location( $atts) {
+	$res = '';
+	
+	// Attributes
+	extract( shortcode_atts(
+		array(
+			'part' => 'both',
+		), $atts )
+	);
+
+	$post_thumbnail_id = get_post_thumbnail_id( $GLOBALS['post']->ID );
+	if ($post_thumbnail_id) {
+		$location = getLocationFromDBorExif($post_thumbnail_id);
+	}
+	if (!$location['hasLocation']) {
+		$location['latitude']='?';
+		$location['longitude']='?';
+	}
+
+	switch ($part) {
+		case 'latitude': $res = $location['latitude']; break;
+		case 'longitude': $res = $location['longitude']; break;
+		default: $res = $location['latitude'].','.$location['longitude'];
+	}
+
+	return $res;	
+}	
+
+function custom_EXIF_locationmap( $atts ) {
+	$res = '';
+	
+	// Attributes
+	extract( shortcode_atts(
+		array(
+			'width' => '300',
+			'height' => '200',
+			'zoom' => '11',
+			'errors' => '0',
+		), $atts )
+	);
+
+	// Code
+	$mapsize = $width.'x'.$height;	
+	
+	$post_thumbnail_id = get_post_thumbnail_id( $GLOBALS['post']->ID );
+	if ($post_thumbnail_id) {
+
+		$location = getLocationFromDBorExif($post_thumbnail_id);
+		if ($location['hasLocation']) {
+			
+			$res  = '<a href="https://www.google.nl/maps/?q='.$location['latitude'].','.$location['longitude'].'&amp;zoom='.$zoom.'" rel="external" title="'.__('click to open a new tab or window with google maps','GPS_MAP_Widget_plugin').'">';
+			$res .=	'<img src="https://maps.googleapis.com/maps/api/staticmap?zoom='.$zoom.'&size='.$mapsize.'&markers=size:mid|'.$location['latitude'].','.$location['longitude'].'" style="width:100%">';
+			$res .= '</a>';
+		} else {
+				if ($errors) {
+					$res = '<p>'.__('There is no GPS information available','GPS_MAP_Widget_plugin').'</p>';
+				}
+		}
+	} else {
+		if ($errors) {
+			$res ='<p>'.__('There is no featured image available','GPS_MAP_Widget_plugin').'</p>';
+		}
+	}
+	
+return $res;
+}
+
+
 class GPS_MAP_Widget extends WP_Widget {
 
 	// constructor
@@ -99,8 +189,8 @@ class GPS_MAP_Widget extends WP_Widget {
 		$width = apply_filters('widget_title', $instance['width']);
 		$height = apply_filters('widget_title', $instance['height']);
 		$zoom = apply_filters('widget_title', $instance['zoom']);
-		$mapsize = $width.'x'.$height;		
-
+	    $errors = true;
+	    
 		echo $before_widget;
 	  
 		// Display the widget
@@ -110,28 +200,13 @@ class GPS_MAP_Widget extends WP_Widget {
 		if ( $title ) {
 			echo $before_title . $title . $after_title;
 		}
-	  
-		$post_thumbnail_id = get_post_thumbnail_id( $GLOBALS['post']->ID );
-		if ($post_thumbnail_id) {
-			$thumbnail=get_attached_file( $post_thumbnail_id, true );
-			$exif = exif_read_data($thumbnail);
-
-			if (is_array($exif["GPSLatitude"]) && is_array($exif["GPSLongitude"])) {
-				$latitude = $this->gps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
-				$longitude = $this->gps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
-				?>
-				
-				<a href="https://www.google.nl/maps/?q=<?php echo $latitude?>,<?php echo $longitude?>&amp;zoom=15" rel="external" title="<?php _e('click to open a new tab or window with google maps','GPS_MAP_Widget_plugin') ?>">
-					<img src="https://maps.googleapis.com/maps/api/staticmap?zoom=<?php echo $zoom ?>&size=<?php echo $mapsize ?>&markers=size:mid|<?php echo $latitude?>,<?php echo $longitude?>" style="width:100%">
-				</a>
-
-				<?php
-			} else {
-				 print '<p>'.__('There is no GPS information available','GPS_MAP_Widget_plugin').'</p>';
-			}
-		} else {
-			print '<p>'.__('There is no featured image available','GPS_MAP_Widget_plugin').'</p>';
-		}
+		print custom_EXIF_locationmap(array(
+				'width'		=> $width,
+				'height'	=> $height,
+				'zoom'		=> $zoom,
+				'errors'	=> true		// in the widget we always some content
+				)
+		);
 
 	echo '</div>';
 	echo $after_widget;
@@ -141,6 +216,12 @@ class GPS_MAP_Widget extends WP_Widget {
 function add_header_code () {
   wp_enqueue_script('GPS_MAP_Widget_js_handler', plugins_url('/js/scripts.js', __FILE__ ), array( 'jquery' ));
 }		
+
+
+
+
+add_shortcode( 'EXIF_locationmap', 'custom_EXIF_locationmap' );
+add_shortcode( 'EXIF_location', 'custom_EXIF_location' );
 
 // register widget
 add_action('widgets_init', create_function('', 'return register_widget("GPS_MAP_Widget");'));
--- readme.txt
+++ readme.txt
@@ -8,15 +8,32 @@ Stable tag: 4.3
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin implements a widget with a static google map which shows the GPS coordinates if those are available in the featured image.
+This plugin implements a widget and a shortcode for a static google map which shows the GPS coordinates if those are available in the featured image.
 
 == Description ==
 
 This plugin implements a widget with a static google map which shows the GPS coordinates if those are available in the featured image.
-It does nothing less, but also nothing more.. 
 
-For a demo see http://www.funsite.eu/gps_map_widget/
+Besides the widget it also adds two shortcodes:
 
+EXIF_locationmap 
+	width: width of the mapimage in px. default 300px
+	height: height of the mapimage in px. default 200px;
+	zoom: googlemaps zoomlevel. Default 11
+	Errors: Show error messages when no exif is available or when there's no featured image. Default is false for the shortcode
+
+	Example: 
+		[EXIF_locationmap width=750 height=300 zoom=11 errors=false]
+		
+EXIF_location
+	part:  'latitude', 'longitude' or 'both'
+	
+	example:
+		[EXIF_location part=both]
+		
+	will return  52.22935055,6.8737411
+
+	
 == Installation ==
 
 1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
@@ -26,20 +43,28 @@ For a demo see http://www.funsite.eu/gps_map_widget/
 == Frequently Asked Questions ==
 
 = Why did you write this widget? =
-
 As an first attempt to write a widget and because I couldn't find one that did what thisone does (and nothing more!)
+
+= Why the shortcodes =
+Guess you can use them for generating your own image from bing maps or alike. Putting a big map in the content has it's charm too.. 
 
 == Screenshots ==
 
-1. Widget as presented to the user (using Alizee theme in this case)
-2. The widget settings
+No screenshots are available. See http://www.funsite.eu/2014/10/dutch-prairies/ "GPS location" for an example.
 
 == Changelog ==
 
+= 1.1 =
+* found coordinates are stored in the database. For now this is done whenever the plugin is shown and only for the post on screen.
+  For those interested I use the meta_key "EXIF_location" containing an array with latitude,longitude and hasLocation. hasLocation is
+  a boolean which is true when the lat/long fields are filled, but empty. (No EXIF info available in the image!)
+
+* Added the shortcode EXIF_location and EXIF_locationmap  
+  
 = 1.0 =
-First release
+* First release
 
 == Upgrade Notice ==
 
-= 1.0 =
-First release
+After the first read the file on disk is left alone. This should speed things up a little and opens up posibilities for other, future, features.
+
